9285a4b6288b1a69732a5a5a4b392515909130ce
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2023-07-14 21:08:27 +0530

    Stack Questions

---
+++ Stack/deleteMidElement.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    
+    void deleteMid(stack<int>&st, int cnt, int &pos){
+        if(st.empty()) return;
+        if(cnt==pos){
+            st.pop();
+            return;
+        }
+        
+        int top = st.top();
+        st.pop();
+        deleteMid(st, cnt+1, pos);
+        
+        st.push(top);
+        
+    }
+    void deleteMid(stack<int>&s, int size)
+    {
+        int pos=ceil((size+1)/2.0);
+        deleteMid(s, 1, pos);
+    }
+};
