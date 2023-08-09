594f21e90d3ffed43eea6dd86772fdd7932ce5b9
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2023-08-10 00:06:38 +0530

    10-08-2023

---
+++ Array/intersectionOfTwoArrays.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    // Function to return the count of the number of elements in the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        unordered_set<int> s;
+        for(int i=0; i<n; i++){
+            s.insert(a[i]);
+        }
+        
+        int cnt=0;
+        for(int i=0; i<m; i++){
+            int key=b[i];
+            if(s.find(key)!=s.end()){
+                cnt++;
+                s.erase(key);
+            }
+        }
+        return cnt;
+    }
+};
+
+// TC = O(n + m) 
+// SC = O(min(n,m))
+
+// GFG Q Link - https://practice.geeksforgeeks.org/problems/intersection-of-two-arrays2404/1
